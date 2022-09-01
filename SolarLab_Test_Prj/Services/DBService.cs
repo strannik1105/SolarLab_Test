41ffcd4f8b5cab9f1e389fb431e56be3e95a3064
strannik1105 <danil.stolpovskikh@gmail.com>
2022-09-01 20:53:13 +0700

    Add DBService

---
+++ SolarLab_Test_Prj/Services/DBService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarLab_Test_Prj.Models;
+
+namespace SolarLab_Test_Prj.Services
+{
+    public class DBService: DbContext
+    {
+        public DBService(DbContextOptions<DBService> options) : base(options) { }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
