7add1a7a13a68c0e7ed0ede093be52b76ca1c84e
strannik1105 <danil.stolpovskikh@gmail.com>
2022-09-01 20:17:42 +0700

    Init

---
+++ SolarLab_Test_Prj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SolarLab_Test_Prj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
