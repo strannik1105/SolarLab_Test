f4c1d0872e55b0c640134a522df429ab42a691b7
strannik1105 <danil.stolpovskikh@gmail.com>
2022-09-01 20:46:57 +0700

    Add Person Entity and IPerson to inheritance

---
+++ SolarLab_Test_Prj/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace SolarLab_Test_Prj.Models
+{
+    public class Person
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime BirthdayDate { get; set; }
+    }
+}
---
+++ SolarLab_Test_Prj/Services/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using SolarLab_Test_Prj.Models;
+
+namespace SolarLab_Test_Prj.Services
+{
+    public interface IPerson
+    {
+        public IEnumerator<Person> Persons { get; set; }
+        
+        public Person GetPerson(int id);
+
+        public IEnumerator<Person> TodayBirthday { get; }
+
+        public IEnumerator<Person> SoonBirthday { get; }
+
+
+    }
+}
